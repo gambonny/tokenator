0271405da2a6f1d4ddfded28f2ce9f9fef910f76
Jonny Gamba <gambonny@gmail.com>
2025-07-23 14:47:40 -0500

    feat: basic class with logger

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { createLogger, type Logger } from "@gambonny/cflo"
+import { env, WorkerEntrypoint } from "cloudflare:workers"
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+const logger = createLogger({
+  level: env.LOG_LEVEL,
+  format: env.LOG_FORMAT,
+})
+
+export class TKNVLDTR extends WorkerEntrypoint {
+  env: Env
+  logger: Logger
+
+  constructor(env: Env, ctx: ExecutionContext) {
+    super(ctx, env)
+    this.env = env
+    this.logger = logger
+  }
+}
